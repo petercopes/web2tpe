60a6408f6e8501c6513ab7ff7bdf61436331ba7c
Pedro Copes <petercopes.96@gmail.com>
2021-11-17 22:05:16 -0300

    agregados archivos con api

---
+++ Controller/ApiProductController.php
@@ -0,0 +1,86 @@
+<?php
+require_once "./Model/ProductModel.php.php";
+require_once "./View/ApiView.php";
+
+class ApiProductController
+{
+
+    private $model;
+    private $view;
+
+    function __construct()
+    {
+        $this->model = new ProductModel();
+        $this->view = new ApiView();
+    }
+
+    function getProducts()
+    {
+        $tareas = $this->model->getProductsFromDB();
+        return $this->view->response($tareas, 200);
+    }
+
+    function getProduct($params = null)
+    {
+        $idProduct = $params[":ID"];
+        $tarea = $this->model->getProductFromDB($idProduct);
+        if ($tarea) {
+            return $this->view->response($tarea, 200);
+        } else {
+            return $this->view->response("No se ha encontrado la categoria con el id $idProduct", 404);
+        }
+    }
+
+    function deleteProduct($params = null)
+    {
+        //agregar verificacion de permiso???
+        $idProduct = $params[":ID"];
+        $product = $this->model->getProductFromDB($idProduct);
+
+        if ($product) {
+            $this->model->deleteProductFromDB($idProduct);
+            return $this->view->response("La categoria con el id= $idProduct fue borrada", 200);
+        } else {
+            return $this->view->response("No se ha encontrado la categoria con el id $idProduct", 404);
+        }
+    }
+
+    function insertProduct()
+    {
+        // obtengo el body del request (json)
+        $body = $this->getBody();
+
+        // TODO: VALIDACIONES -> 400 (Bad Request)
+
+        $id = $this->model->addProductToDB($body->name, $body->description, $body->price, $body->categoryId);
+        if ($id != 0) {
+            $this->view->response("La categoria se insertó con el id= $id", 200);
+        } else {
+            $this->view->response("La categoria no se pudo insertar", 500);
+        }
+    }
+
+    function updateProduct($params = null)
+    {
+        $idProduct = $params[':ID'];
+        $body = $this->getBody();
+
+        // TODO: VALIDACIONES -> 400 (Bad Request)
+        $product = $this->model->getProductFromDB($idProduct);
+        if ($product) {
+            $this->model->updateProductOnDB($idProduct, $body->name, $body->description, $body->price);
+            $this->view->response("La categoria se actualizó correctamente", 200);
+        } else {
+            return $this->view->response("No se ha encontrado la categoria con el id $idProduct", 404);
+        }
+    }
+
+    /**
+     * Devuelve el body del request
+     */
+    private function getBody()
+    {
+        $bodyString = file_get_contents("php://input");
+        return json_decode($bodyString);
+    }
+}
---
+++ Helper/AuthApiHelper.php
@@ -0,0 +1,93 @@
+<?php
+
+function base64url_encode($data)
+{
+    return rtrim(strtr(base64_encode($data), '+/', '-_'), '=');
+}
+
+class AuthApiHelper
+{
+    private $key;
+
+    function __construct()
+    {
+        $this->key = "Hola123ab";
+    }
+
+    function getBasic()
+    {
+        $header = $this->getHeader();
+        // Basic base64(user:pass)
+        // ^
+        if (strpos($header, "Basic ") === 0) {
+            // base64(user:pass)
+            $usrpass = explode(" ", $header)[1];
+            // user:pass
+            $usrpass = base64_decode($usrpass);
+            $usrpass = explode(":", $usrpass);
+            if (count($usrpass) == 2) {
+                $user = $usrpass[0];
+                $pass = $usrpass[1];
+                return array(
+                    "user" => $user,
+                    "pass" => $pass
+                );
+            }
+        }
+        return null;
+    }
+
+    function getUser()
+    {
+        $header = $this->getHeader();
+        // Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOjEsIm5hbWUiOiJ1c3VhcmlvMSIsInJvbCI6WyJhZG1pbiIsIm90aGVyIl19.6mRGSZGxCeBuQWp5daMmkNbNMWeaQFzF77a7SCqNXuo
+        if (strpos($header, "Bearer ") === 0) {
+            $token = explode(" ", $header)[1];
+            $parts = explode(".", $token);
+            if (count($parts) === 3) {
+                $header = $parts[0];
+                $payload = $parts[1];
+                $signature = $parts[2];
+                $new_signature = hash_hmac('SHA256', "$header.$payload", $this->key, true);
+                $new_signature = base64url_encode($new_signature);
+                if ($signature == $new_signature) {
+                    $payload = base64_decode($payload);
+                    $payload = json_decode($payload);
+                    if (true/*$payload->exp < $now*/) {
+                        return $payload;
+                    }
+                }
+            }
+        }
+        return null;
+    }
+
+    public function createToken($user)
+    {
+        $header = array(
+            'alg' => 'HS256',
+            "typ" => 'JWT'
+        );
+        $payload = array(
+            "sub" => 1,
+            "name" => $user["user"],
+            "rol" => ["admin", "other"]
+        );
+        $header = base64url_encode(json_encode($header));
+        $payload = base64url_encode(json_encode($payload));
+        $signature = hash_hmac('SHA256', "$header.$payload", $this->key, true);
+        $signature = base64url_encode($signature);
+        return "$header.$payload.$signature";
+    }
+
+    function getHeader()
+    {
+        if (isset($_SERVER["REDIRECT_HTTP_AUTHORIZATION"])) {
+            return $_SERVER["REDIRECT_HTTP_AUTHORIZATION"];
+        }
+        if (isset($_SERVER["HTTP_AUTHORIZATION"])) {
+            return $_SERVER["HTTP_AUTHORIZATION"];
+        }
+        return null;
+    }
+}
