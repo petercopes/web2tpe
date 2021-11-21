4897c8963834ca68ec6ec15016738f8e22f81716
petercopes <64164208+petercopes@users.noreply.github.com>
2021-11-21 18:04:31 -0300

    Comments (#19)
    * agregados archivos con api
    
    * added js files
    
    * made changes in routing
    
    * updated forms with http methods
    
    * added user tab
    
    * creada la seccion de comentarios
    
    * added csr functionality
    
    * added comments into product detail

--- route-api.php
+++ route-api.php
@@ -8,17 +8,19 @@ require_once 'Controller/ApiUserController.php';
 $router = new Router();
 
 // define la tabla de ruteo
-$router->addRoute('categorias', 'GET', 'ApiCategoryController', 'obtenerTareas');
-$router->addRoute('categorias/:ID', 'GET', 'ApiCategoryController', 'obtenerTarea');
-$router->addRoute('categorias/:ID', 'DELETE', 'ApiCategoryController', 'eliminarTarea');
-$router->addRoute('categorias', 'POST', 'ApiCategoryController', 'insertarTarea');
-$router->addRoute('categorias/:ID', 'PUT', 'ApiCategoryController', 'actualizarTarea');
 
-$router->addRoute('productos', 'GET', 'ApiProductController', 'obtenerTareas');
-$router->addRoute('productos/:ID', 'GET', 'ApiProductController', 'obtenerTarea');
-$router->addRoute('productos/:ID', 'DELETE', 'ApiProductController', 'eliminarTarea');
-$router->addRoute('productos', 'POST', 'ApiProductController', 'insertarTarea');
-$router->addRoute('productos/:ID', 'PUT', 'ApiProductController', 'actualizarTarea');
+$router->addRoute('categorias', 'GET', 'ApiCategoryController', 'obtenerCategorias');
+$router->addRoute('categorias/:ID', 'GET', 'ApiCategoryController', 'obtenerCategoria');
+$router->addRoute('categorias/:ID', 'DELETE', 'ApiCategoryController', 'eliminarCategoria');
+$router->addRoute('categorias', 'POST', 'ApiCategoryController', 'insertarCategoria');
+$router->addRoute('categorias/:ID', 'PUT', 'ApiCategoryController', 'actualizarCategoria');
+
+$router->addRoute('productos', 'GET', 'ApiProductController', 'obtenerProductos');
+$router->addRoute('productos/:ID', 'GET', 'ApiProductController', 'obtenerProducts');
+$router->addRoute('productos/:ID', 'DELETE', 'ApiProductController', 'eliminarProducto');
+$router->addRoute('productos', 'POST', 'ApiProductController', 'insertarProducto');
+$router->addRoute('productos/:ID', 'PUT', 'ApiProductController', 'actualizarProducto');
+
 
 
 $router->addRoute('users/token', 'GET', 'ApiUserController', 'obtenerToken');
