4220db659dd112d09f9b82380e3c76c26f0f660f
Dulce Villabona <villabonadulce@gmail.com>
2021-11-22 16:18:10 -0300

    comments list restored

--- templates_c/ab77ea66e13da49a9c9f2c6891dfccc65d4163f8_0.file.commentList.tpl.php
+++ templates_c/ab77ea66e13da49a9c9f2c6891dfccc65d4163f8_0.file.commentList.tpl.php
@@ -1,18 +1,18 @@
 <?php
-/* Smarty version 3.1.39, created on 2021-11-22 20:10:31
+/* Smarty version 3.1.39, created on 2021-11-22 20:15:41
   from 'C:\xampp\htdocs\web2tpe\templates\commentList.tpl' */
 
 /* @var Smarty_Internal_Template $_smarty_tpl */
 if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   'version' => '3.1.39',
-  'unifunc' => 'content_619beb2741b7c2_14744865',
+  'unifunc' => 'content_619bec5daf6ff0_14516881',
   'has_nocache_code' => false,
   'file_dependency' => 
   array (
     'ab77ea66e13da49a9c9f2c6891dfccc65d4163f8' => 
     array (
       0 => 'C:\\xampp\\htdocs\\web2tpe\\templates\\commentList.tpl',
-      1 => 1637608227,
+      1 => 1637608498,
       2 => 'file',
     ),
   ),
@@ -20,12 +20,8 @@ if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   array (
   ),
 ),false)) {
-function content_619beb2741b7c2_14744865 (Smarty_Internal_Template $_smarty_tpl) {
-?>
-    <div id="app">
-        <ul id="lista-tareas" class="list-group">
-            
-        </ul>
-    </div>
-<?php }
+function content_619bec5daf6ff0_14516881 (Smarty_Internal_Template $_smarty_tpl) {
+?><div id="app">
+
+</div><?php }
 }
