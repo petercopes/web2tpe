1242ac4b6b22c619ede7be38609c36aa28aaf982
Dulce Villabona <villabonadulce@gmail.com>
2021-11-24 22:22:48 -0300

    handling category delete error

--- Model/CategoryModel.php
+++ Model/CategoryModel.php
@@ -1,33 +1,39 @@
 <?php
 
-class CategoryModel{
+class CategoryModel
+{
 
     private $db;
-    function __construct(){
-         $this->db = new PDO('mysql:host=localhost;'.'dbname=tp-web-2;charset=utf8', 'root', '');
+    function __construct()
+    {
+        $this->db = new PDO('mysql:host=localhost;' . 'dbname=tp-web-2;charset=utf8', 'root', '');
     }
 
-    function getCategories(){
-        $sentencia = $this->db->prepare( "select * from category");
+    function getCategories()
+    {
+        $sentencia = $this->db->prepare("select * from category");
         $sentencia->execute();
         $categories = $sentencia->fetchAll(PDO::FETCH_OBJ);
         return $categories;
-    } 
+    }
 
-    function addCategory($name, $description){
+    function addCategory($name, $description)
+    {
         $sentencia = $this->db->prepare("INSERT INTO category(name, description) VALUES(?, ?)");
         $sentencia->bindParam(1, $name, PDO::PARAM_STR, 50);
         $sentencia->bindParam(2, $description, PDO::PARAM_STR, 50);
         $sentencia->execute();
     }
 
-    function deleteCategory($id){
+    function deleteCategory($id)
+    {
         $sentencia = $this->db->prepare("DELETE FROM category WHERE id_category=?");
         $sentencia->bindParam(1, $id, PDO::PARAM_INT);
         $sentencia->execute();
     }
 
-    function getCategoryProducts($id){
+    function getCategoryProducts($id)
+    {
         $sentencia = $this->db->prepare("select * from product WHERE id_category=?");
         $sentencia->bindParam(1, $id, PDO::PARAM_INT);
         $sentencia->execute();
@@ -35,7 +41,8 @@ class CategoryModel{
         return $products;
     }
 
-    function getCategory($id){
+    function getCategory($id)
+    {
         $sentencia = $this->db->prepare("SELECT * FROM category WHERE id_category=?");
         $sentencia->bindParam(1, $id, PDO::PARAM_INT);
         $sentencia->execute();
@@ -43,7 +50,8 @@ class CategoryModel{
         return $category;
     }
 
-    function editCategory($id, $name, $description){
+    function editCategory($id, $name, $description)
+    {
         $sentencia = $this->db->prepare("UPDATE category SET name=?, description=? WHERE id_category=?");
         $sentencia->bindParam(1, $name, PDO::PARAM_STR, 50);
         $sentencia->bindParam(2, $description, PDO::PARAM_STR, 50);
